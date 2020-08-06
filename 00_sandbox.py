b1d3dd6e1555a68f2654c3b0d9519242c743d4cd
Woo-Jin Jeon <jeonw70450@masseyhigh.school.nz>
2020-08-06 20:20:37 +0000

    Updated Sandbox to ask for recipe name and defined not_blank

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,1 +1,21 @@
-print("hello world")
+#This is a sandbox area where I test out my code.
+
+
+#Not Blank function goes here
+def not_blank(question):
+
+    valid = False
+    while not valid:
+      response = input(question)
+    
+      if response == "":
+        continue 
+      else:
+       return response
+
+
+
+#The main code
+print("Today we are going to make food")
+recipe_name= not_blank("What is the recipe name?")
+print("You are making {}".format(recipe_name))
